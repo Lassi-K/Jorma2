92b43167ef60b265ce2239a78a959208c7250540
Lassi-K <lassiheikkivaltteri@gmail.com>
2025-01-31 21:29:45 +0200

    Add files via upload

---
+++ jotain.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Jorma on vankilassa, koska hän virtsasi maakaivon kannelle humalassa"
+echo "Auta oikeusmurhan uhrina toimivaa Jormaa pakenemaan vankilasta"
+echo "Voit ohjata Jormaa komennoilla, kuten <katso ympärillesi> <ota esine> <käytä esinettä> <mene> jne."
+echo "Olet Jorma"
+
+paikka=selli
+
+echo -n ">> "
+read input
+
+while [[ $paikka == "selli" ]];
+do
+	if [[ $input == "katso ympärillesi" ]]; then
+		unset input
+
+		echo "Olet sellissä, jonka toisessa seinässä  on ikkuna ja toisessa ovi."
+		echo "Istut kapealla sängyllä, jota vastapäätä on WC pönttö"
+
+		echo -n ">> "
+		read input
+
+	
+	elif [[ $input == "mene paskalle" ]]; then
+		unset input
+		echo "Paskalla"
+		sleep 3m
+
+		echo "Nytton suali tyhjä"
+
+		echo -n ">> "
+		read input
+	fi
+done
